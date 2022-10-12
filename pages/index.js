1b3f8c778b6564c87a4d77c13162a422f9427fad
flash-of-genius <amhdaar@gmail.com>
2022-10-12 10:39:00 +0100

    pages\index.js

--- pages/index.js
+++ pages/index.js
@@ -4,11 +4,13 @@ const Home = () => {
   return (
     <>
       heroBanner
-      <div>
+      <div className="products-heading">
         <h2>Best Selling Product</h2>
         <p>Speakers of many variations</p>
       </div>
-      <div>{['Product 1', 'Product 2'].map((product) => product)}</div>
+      <div className="products-container">
+        {['Product 1', 'Product 2'].map((product) => product)}
+      </div>
       Footer
     </>
   );
