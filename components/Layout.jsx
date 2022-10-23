cbd5359151bd8aae894180d412e7830794a61f9d
flash-of-genius <amhdaar@gmail.com>
2022-10-23 08:34:08 +0100

    components\Layout.jsx

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -1,7 +1,21 @@
 import React from 'react';
+import Head from 'next/head';
+import NavBar from './NavBar';
+import Footer from './Footer';
 
 const Layout = () => {
-  return <div>Layout</div>;
+  return (
+    <div className="layout">
+      <Head>
+        <title>JS Mastery Store</title>
+      </Head>
+      <header>
+        <NavBar />
+      </header>
+      <main className="main-container">EMPTY</main>
+      <Footer />
+    </div>
+  );
 };
 
 export default Layout;
