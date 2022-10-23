bd7ae48ff0a156226de8f66f8abc20ac38fdb982
flash-of-genius <amhdaar@gmail.com>
2022-10-23 15:10:20 +0100

    components\NavBar.jsx

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
+import Link from 'next/link';
+import { AiOutlineShopping } from 'react-icons/ai';
 
 const NavBar = () => {
-  return <div>NavBar</div>;
+  return (
+    <div className="navbar-container">
+      <p className="logo">
+        <Link href="/">JSM Headphones</Link>
+      </p>
+      <button className="cart-icon" type="button" onClick="">
+        <AiOutlineShopping />
+        <span className="cart-item-qty">1</span>
+      </button>
+    </div>
+  );
 };
 
 export default NavBar;
